2f419359737f0fff31e2fd4a916d04b59f220cba
ZhmakinAA <ZhmakinAA@hotmail.ru>
2023-05-15 12:35:49 +0300

    Add project files.

---
+++ lab7/Book.cs
@@ -0,0 +1,32 @@
+﻿namespace lab7_ex6
+{
+    internal class Book : IComparable
+    {
+        private string title;
+        private string author;
+        private int pages;
+        private int year;
+
+        public Book(string title, string author, int pages, int year)
+        {
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+            this.year = year;
+        }
+
+        int IComparable.CompareTo(object? obj)
+        {
+            Book it = (Book)obj;
+            if (this.year == it.year) return 0;
+            else if (this.year > it.year) return 1;
+            else return -1;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Книга: {title}\nАвтор: {author}\nГод издания: {year}\nстр.: {pages}\n");
+
+        }
+    }
+}
---
+++ lab7/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace lab7_ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book[] book = new Book[5];
+
+            book[0] = new Book("Мастер и Маргарита", "М.Булгаков", 842, 1967);
+            book[1] = new Book("12 стульев", "Е.Ильф", 448, 1928);
+            book[2] = new Book("Мертвые души", "Н.Гоголь", 560, 2022);
+            book[3] = new Book("Братья карамазовы", "Ф.Достоевский", 734, 1989);
+            book[4] = new Book("Тихий дон", "М.Шолохов", 978, 1975);
+
+            Array.Sort(book);
+
+            for (int i = 0; i < book.Length; i++)
+            {
+                book[i].Show();
+            }
+        }
+    }
+}
